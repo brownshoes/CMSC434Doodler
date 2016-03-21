54f77aabdf050af5249dd2c2e978ce154340c71b
Me <fezzler@gmail.com>
2016-03-21 17:32:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/me/cmsc434doodler/DoodleView.java
@@ -0,0 +1,196 @@
+package com.example.me.cmsc434doodler;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Me on 3/5/2016.
+ */
+public class DoodleView extends View {
+
+
+    private class Data {
+        Path storePath;
+        int storeColor;
+        float storeStroke;
+        int storeAlpha;
+
+        Data(Path x, int color, float stroke, int alpha){
+            this.storePath = x;
+            this.storeColor = color;
+            this.storeStroke = stroke;
+            this.storeAlpha = alpha;
+
+        }
+
+    }
+    private ArrayList<Data> store = new ArrayList<Data>();
+
+    private int element = 0;
+
+    private Paint paintDoodle = new Paint();
+    private Path paintPath = new Path();
+
+    //defaults
+    private int currentColor = Color.BLACK;
+    private float currentStroke = 4;
+    private int currentAlpha = 30;
+
+    private Bitmap canvasBitmap;
+    //private Canvas drawCanvas;
+    //private Paint canvasPaint = new Paint(Paint.DITHER_FLAG);;
+
+    public DoodleView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public DoodleView(Context context,AttributeSet attrs ) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public DoodleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
+    }
+
+    private void init(AttributeSet attrs, int defStyleAttr){
+        paintDoodle.setColor(currentColor);
+        paintDoodle.setStyle(Paint.Style.STROKE);
+    }
+
+    public void changeColor(int color) {
+
+        invalidate();
+        currentColor = color;
+        paintDoodle.setColor(color);
+    }
+
+    public void changeStroke(float stroke){
+
+        invalidate();
+        currentStroke = stroke;
+        paintDoodle.setStrokeWidth(stroke);
+    }
+
+    public void changeAlpha(int alpha){
+
+        invalidate();
+        currentAlpha = alpha;
+        paintDoodle.setAlpha(alpha);
+    }
+
+    public void undo(){
+
+        //store.remove(store.size() - 1);
+
+        if (element > 0){
+
+            element--;
+            invalidate();
+        }   
+
+    }
+
+    public void redo(){
+
+       if(element < store.size() ){
+
+            element++;
+            invalidate();
+       }
+    }
+
+    public void clear(){
+
+        store.clear();
+        element = 0;
+        invalidate();
+    }
+/*
+    //size assigned to view
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+*/
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        //canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+
+        //int length = store.size();
+
+        //for(int count = 0; count < length; count++){
+        for(int count = 0; count < element; count++){
+
+            Path savePath = store.get(count).storePath;
+            int saveColor = store.get(count).storeColor;
+            float saveStroke = store.get(count).storeStroke;
+            int saveAlpha = store.get(count).storeAlpha;
+
+            paintDoodle.setColor(saveColor);
+            paintDoodle.setStrokeWidth(saveStroke);
+            paintDoodle.setAlpha(saveAlpha);
+            canvas.drawPath(savePath, paintDoodle);
+
+        }
+
+        paintDoodle.setStrokeWidth(currentStroke);
+        paintDoodle.setAlpha(currentAlpha);
+        paintDoodle.setColor(currentColor);
+        canvas.drawPath(paintPath, paintDoodle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        float touchX = motionEvent.getX();
+        float touchY = motionEvent.getY();
+
+        switch(motionEvent.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                paintPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                paintPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+
+                //paintPath.lineTo(touchX, touchY);
+                //drawCanvas.drawPath(paintPath, paintDoodle);
+
+                if(element < store.size()) {                    
+
+                    while (store.size() > element){
+
+                        store.remove(element);
+                    }
+
+                }
+
+                store.add(new Data(new Path(paintPath), currentColor, currentStroke, currentAlpha));
+                element++;
+                paintPath.reset();
+                break;
+        }
+
+        invalidate();
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/me/cmsc434doodler/MainActivity.java
@@ -0,0 +1,254 @@
+package com.example.me.cmsc434doodler;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private int colorCount = 0;
+    private DoodleView doodleView;
+
+    private ImageButton black;
+    private ImageButton red;
+    private ImageButton yellow;
+    private ImageButton green;
+
+    private ImageButton blue;
+    private ImageButton magenta;
+    private ImageButton cyan;
+    private ImageButton white;
+
+
+    private Button undoButton;
+    private Button clearButton;
+    private Button redoButton;
+
+    private SeekBar sizeStroke;
+    private SeekBar opacity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        doodleView = (DoodleView)findViewById(R.id.doodle);
+
+        black = (ImageButton) findViewById(R.id.Black);
+        red = (ImageButton) findViewById(R.id.Red);
+        yellow = (ImageButton) findViewById(R.id.Yellow);
+        green = (ImageButton) findViewById(R.id.Green);
+
+        blue = (ImageButton) findViewById(R.id.Blue);
+        cyan = (ImageButton) findViewById(R.id.Cyan);
+        magenta = (ImageButton) findViewById(R.id.Magenta);
+        white = (ImageButton) findViewById(R.id.White);
+
+        undoButton = (Button) findViewById(R.id.undo);
+        redoButton = (Button) findViewById(R.id.redo);
+        clearButton = (Button) findViewById(R.id.clear);
+
+        sizeStroke = (SeekBar) findViewById(R.id.sizeSeek);
+        opacity = (SeekBar) findViewById(R.id.opacity);
+
+        opacity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int value = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                value = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                doodleView.changeAlpha(value);
+
+            }
+        });
+
+        sizeStroke.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            float value = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                value = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                doodleView.changeStroke(value);
+
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doodleView.clear();
+            }
+        });
+
+
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doodleView.undo();
+            }
+        });
+
+        redoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doodleView.redo();
+            }
+        });
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.RED);
+
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.GREEN);
+
+            }
+        });
+
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.YELLOW);
+
+            }
+        });
+
+        black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.BLACK);
+
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.BLUE);
+
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.GREEN);
+
+            }
+        });
+
+        magenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.MAGENTA);
+
+            }
+        });
+
+        cyan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.CYAN);
+
+            }
+        });
+
+        white.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doodleView.changeColor(Color.WHITE);
+
+            }
+        });
+
+    }
+/*
+    //user clicked paint
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currPaint){
+
+            String color = view.getTag().toString();
+            doodleView.changeColor(color);
+
+        }
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
